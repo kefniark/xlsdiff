bf03a167a593e548cf5eb2ccf63ae380fba20430
Kevin Destrem <kefniark@gmail.com>
2019-02-06 20:19:41 +0900

    simplify output

--- index.js
+++ index.js
@@ -13,25 +13,14 @@ function xlsx2txt(xlsxfile) {
                 txt +=  obj[tab].name + ' | ';
                 for (var x = 0; x < csvobj[y].length; ++x) {
                     if (csvobj[y][x] != undefined) {
-                        var str = csvobj[y][x].toString();
-
-                        if (str.indexOf(',') >= 0) {
-                            txt += '"';
-                            txt += csvobj[y][x];
-                            txt += '"';
-                        }
-                        else {
-                            txt += csvobj[y][x];
-                        }
+                        txt += csvobj[y][x];
                     }
 
                     if (x < csvobj[y].length - 1) {
-                        txt += ' | ';
+                        txt += '| ';
                     }
                 }
-                txt += '\r\n';
 
-                // use multiple console.log big ones can be truncated on mac
                 console.log(txt);
                 txt = '';
             }
